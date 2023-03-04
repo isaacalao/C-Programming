2bfd091a4df4598e8d13d9267076b769c5030e16
IsaacAlao <isaacmobolajialao@gmail.com>
2023-03-04 12:21:01 -0500

    Added input.txt and modified filecopyingpart1

--- src/Ch1/FileCopyingPart1.c
+++ src/Ch1/FileCopyingPart1.c
@@ -15,6 +15,6 @@ int main(void) {
       printf("%d", c != EOF);
 
     //Check if the the last input is equal to EOF
-    printf("\nThe series of 1s means that the expr getchar() != EOF is true.\n");
+    printf("\nThe series of 1s means that the expr getchar() != EOF is true for each character that is != to EOF.\n");
     printf("But this %d means that EOF was found in the given input stream and the expr above was false.", c != EOF);
 }
