760f311563d327b54aeebf13302e2520972aafd8
IsaacAlao <isaacmobolajialao@gmail.com>
2023-03-04 12:15:07 -0500

    Changed a couple files here

---
+++ src/Ch1/FileCopyingPart1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Author: Isaac Alao
+ * Chapter: I
+ * Exercise: Verify that the expression getchar() != EOF is 0 or 1
+ * Purpose: Reinforce use of getchar() and putchar()
+ *
+ **/
+
+int main(void) {
+    int c;
+
+    //Loop through all the characters
+    while ((c = getchar()) != EOF)
+      printf("%d", c != EOF);
+
+    //Check if the the last input is equal to EOF
+    printf("\nThe series of 1s means that the expr getchar() != EOF is true.\n");
+    printf("But this %d means that EOF was found in the given input stream and the expr above was false.", c != EOF);
+}
---
+++ src/Ch1/FileCopyingPart2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Author: Isaac Alao
+ * Chapter: I
+ * Exercise: Write a program to print the value of EOF
+ * Purpose: Reinforce use of getchar() and putchar()
+ *
+ **/
+
+int main(void) {
+ // Did you know that EOF is equivalent to Ctrl-D?
+ printf("Value of EOF: %i", EOF);
+}
