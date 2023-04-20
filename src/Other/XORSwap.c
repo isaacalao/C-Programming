d099ab19ae29b8a930ff9d44b43a08a5100f430c
IsaacAlao <isaacmobolajialao@gmail.com>
2023-04-20 13:57:34 -0400

    Changes to be committed: 	new file:   XORSwap.c

---
+++ src/Other/XORSwap.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Source: Jacob Sorber
+
+int main() {
+
+int x = 55, y = 32;
+
+ printf(
+  "Before the swap:\n"
+  "x = %d\n"
+  "y = %d\n\n",
+   x, y
+ );
+
+ /*
+  It's important to have (x = x ^ y) on far left so the result of the operation can xor (y = x ^ y) 
+  [which has been successfully swapped] to acquire the former value of y
+ */
+ x = ((x = x ^ y) ^ (y = x ^ y));
+
+ printf(
+  "After the swap:\n"
+  "x = %d\n"
+  "y = %d\n",
+   x, y
+ );
+ return 0;
+}
