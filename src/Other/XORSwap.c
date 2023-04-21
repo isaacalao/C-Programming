cd9c2a948a9a9d4955dc39aa96c8ad3cf27379a4
IsaacAlao <88806554+IsaacAlao@users.noreply.github.com>
2023-04-21 13:04:26 -0400

    Update XORSwap.c

--- src/Other/XORSwap.c
+++ src/Other/XORSwap.c
@@ -15,8 +15,8 @@ int x = 55, y = 32;
  );
 
  /*
-  It's important to have (x = x ^ y) on far left so the result of the operation can xor (y = x ^ y) 
-  [which has been successfully swapped] to acquire the former value of y
+  It's important to have (x = x ^ y) on the far left so the result of the operation can xor (y = x ^ y) 
+  [which has been successfully swapped] to acquire the former value of y and assign it to x
  */
  x = ((x = x ^ y) ^ (y = x ^ y));
 
