e1b6651264a304e60ee72d2fe927166245c1405c
IsaacAlao <isaacmobolajialao@gmail.com>
2023-06-25 19:39:21 -0400

    Changes to be committed: 	modified:   ../../scripts/automate.sh

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -15,7 +15,7 @@ do
   do
     echo -e "\e[1;32m${file}\e[0m:";
     # i added an input file so some programs that require input can read from it
-    cc $file && bash geninput 2> dev/null | ./a.out && rm $_; #cc $file && ./a.out < input && rm $_;
+    cc $file && bash geninput 2> /dev/null | ./a.out && rm $_; #cc $file && ./a.out < input && rm $_;
     echo -e "\n"; sleep 0.25;
   done
   cd ..;
