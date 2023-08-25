904e58fc36ca9926bfb4beaf8822bdc9c615a64b
IsaacAlao <88806554+IsaacAlao@users.noreply.github.com>
2023-08-25 12:42:27 -0400

    Changes to be committed: 	modified:   scripts/automate.sh

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -7,16 +7,16 @@ do
   # .c file, echo that directory name or if the directory doesn't contain a .c file then continue on to the next iteration
   [[ -z "$(ls $dir | grep ".c")" ]] && continue;
 
-  echo -e "\e[4;32m$dir\e[0m\n";
+  printf "\e[4;32m$dir\e[0m\n";
   cd $dir;
 
   # echo the name of the .c file, compile, execute, remove, sleep, and repeat
   for file in *.c;
   do
-    echo -e "\e[1;32m${file}\e[0m:";
+    printf "\e[1;32m${file}\e[0m:\n";
     # i added an input file so some programs that require input can read from it
     cc $file && bash geninput 2> /dev/null | ./a.out && rm $_; #cc $file && ./a.out < input && rm $_;
-    echo -e "\n"; sleep 0.25;
+    printf "\n"; sleep 0.25;
   done
   cd ..;
 done
